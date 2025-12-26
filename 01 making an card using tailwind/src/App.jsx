a8b4278a98e403b0a51f2a7224d53dbc0574166d
harshad diwate <harshaddiwate85@gmail.com>
2025-12-26 23:14:32 +0530

    make a card in tailwind css

--- 01 making an card using tailwind/src/App.jsx
+++ 01 making an card using tailwind/src/App.jsx
@@ -3,11 +3,67 @@ import './App.css'
 function App() {
 
 
-  return (
-    <>
-<h1 className="text-red-600 border-2 rounded-4xl p-4">Hi this is my first react site</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-red-600 border-2 rounded-4xl p-4 text-xs"> This is my First Card with @Tailwind CSS</h1>
+            <div className="flex gap-3">
+                <div className="mt-4 max-w-50 bg-gray-100 rounded-3xl">
+                    <img className="max-h-48 object-cover rounded-xl"
+                         src="http://imgs.search.brave.com/KvyCGcqE0DStMyoW2X66uSgk5WEGJXv1pG_plAunraw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvbmFy/dXRvLXBpY3R1cmVz/LXc1eWFhMHNjcm9s/NGxncGsuanBn"
+                         alt="naruto"></img>
+
+                    <div>
+                        <h2 className="text-black">Card Title</h2>
+                        <p className="text-black text-xs px-2" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus fugit ipsum nobis odit, quidem
+                            repellendus.</p>
+                    </div>
+                    <button className="mt-2 mb-2 bg-blue-700 p-2 rounded-xl hover:bg-blue-800">Buy now</button>
+                </div>
+
+                <div className="mt-4 max-w-50 bg-gray-100 rounded-3xl">
+                    <img className="max-h-48 object-cover rounded-xl"
+                         src="http://imgs.search.brave.com/KvyCGcqE0DStMyoW2X66uSgk5WEGJXv1pG_plAunraw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvbmFy/dXRvLXBpY3R1cmVz/LXc1eWFhMHNjcm9s/NGxncGsuanBn"
+                         alt="naruto"></img>
+
+                    <div>
+                        <h2 className="text-black">Card Title</h2>
+                        <p className="text-black text-xs px-2" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus fugit ipsum nobis odit, quidem
+                            repellendus.</p>
+                    </div>
+                    <button className="mt-2 mb-2 bg-blue-700 p-2 rounded-xl hover:bg-blue-800">Buy now</button>
+                </div>
+
+                <div className="mt-4 max-w-50 bg-gray-100 rounded-3xl">
+                    <img className="max-h-48 object-cover rounded-xl"
+                         src="http://imgs.search.brave.com/KvyCGcqE0DStMyoW2X66uSgk5WEGJXv1pG_plAunraw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvbmFy/dXRvLXBpY3R1cmVz/LXc1eWFhMHNjcm9s/NGxncGsuanBn"
+                         alt="naruto"></img>
+
+                    <div>
+                        <h2 className="text-black">Card Title</h2>
+                        <p className="text-black text-xs px-2" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus fugit ipsum nobis odit, quidem
+                            repellendus.</p>
+                    </div>
+                    <button className="mt-2 mb-2 bg-blue-700 p-2 rounded-xl hover:bg-blue-800">Buy now</button>
+                </div>
+
+                <div className="mt-4 max-w-50 bg-gray-100 rounded-3xl">
+                    <img className="max-h-48 object-cover rounded-xl"
+                         src="http://imgs.search.brave.com/KvyCGcqE0DStMyoW2X66uSgk5WEGJXv1pG_plAunraw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvbmFy/dXRvLXBpY3R1cmVz/LXc1eWFhMHNjcm9s/NGxncGsuanBn"
+                         alt="naruto"></img>
+
+                    <div>
+                        <h2 className="text-black">Card Title</h2>
+                        <p className="text-black text-xs px-2" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus fugit ipsum nobis odit, quidem
+                            repellendus.</p>
+                    </div>
+                    <button className="mt-2 mb-2 bg-blue-700 p-2 rounded-xl hover:bg-blue-800">Buy now</button>
+                </div>
+
+            </div>
+
+
+        </>
+    )
 }
 
 export default App
