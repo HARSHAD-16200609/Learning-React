e535f359afba547b04648f3f3869feddc9366c70
harshad diwate <harshaddiwate85@gmail.com>
2025-12-26 22:37:20 +0530

    initial commit

---
+++ 01 making an card using tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<h1 className="text-red-600 border-2 rounded-4xl p-4">Hi this is my first react site</h1>
+    </>
+  )
+}
+
+export default App
