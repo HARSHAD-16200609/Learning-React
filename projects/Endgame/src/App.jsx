e87fe1151e954f379f5d8f2c36159dcc34c6cc8d
harshad diwate <harshaddiwate85@gmail.com>
2026-01-03 19:10:12 +0530

    feat: Initialize a new React project with the "Assembly: Endgame" game UI and styling.

---
+++ projects/Endgame/src/App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main-cont">
+        <h1>Assembly: Endgame</h1>
+        <h3>Guess the ward in under 8 attempts to :keep the</h3>
+        <h3>programming world safe from Assembly!!</h3>
+        <div className="greeting">
+          <h1>You Win!!</h1>
+          <h2>Well done 🎉!!</h2>
+        </div>
+        <div className="lang-cont">
+          <span className='Javascript'>Javascript</span>
+          <span className='Typescript'>Typescript</span>
+          <span className='React'>React</span>
+          <span className='HTML'>HTML</span>
+          <span className='CSS'>CSS</span>
+          <span className='Python'>Python</span>
+          <span className='NodeJs'>NodeJs</span>
+          <span className='Ruby '>Ruby</span>
+          <span className='Assembly '>Assembly</span>
+          <div className="input-cont">
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+            <input type="text" maxLength="1"></input>
+          </div>
+          <div class="alpha-cont">
+            <div class="alphabet">A</div>
+            <div class="alphabet">B</div>
+            <div class="alphabet">C</div>
+            <div class="alphabet">D</div>
+            <div class="alphabet">E</div>
+            <div class="alphabet">F</div>
+            <div class="alphabet">G</div>
+            <div class="alphabet">H</div>
+            <div class="alphabet">I</div>
+            <div class="alphabet">J</div>
+            <div class="alphabet">K</div>
+            <div class="alphabet">L</div>
+            <div class="alphabet">M</div>
+            <div class="alphabet">N</div>
+            <div class="alphabet">O</div>
+            <div class="alphabet">P</div>
+            <div class="alphabet">Q</div>
+            <div class="alphabet">R</div>
+            <div class="alphabet">S</div>
+            <div class="alphabet">T</div>
+            <div class="alphabet">U</div>
+            <div class="alphabet">V</div>
+            <div class="alphabet">W</div>
+            <div class="alphabet">X</div>
+            <div class="alphabet">Y</div>
+            <div class="alphabet">Z</div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
