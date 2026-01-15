14ab70fb2baa3aa6ea3771d87a7fdb39a38c6f46
harshad diwate <harshaddiwate85@gmail.com>
2026-01-16 00:51:27 +0530

    feat: refactor App and MovieCard components for improved movie data handling and layout

--- projects/movieRecomendation/src/App.jsx
+++ projects/movieRecomendation/src/App.jsx
@@ -1,63 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import MovieCard from "./components/movieCard.jsx";
-
-const movies = [
-  {
-    title: "Inception",
-    releaseDate: "2010-07-16",
-    poster: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
-  },
-  {
-    title: "Interstellar",
-    releaseDate: "2014-11-07",
-    poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-  },
-  {
-    title: "The Dark Knight",
-    releaseDate: "2008-07-18",
-    poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
-  },
-  {
-    title: "Avengers: Endgame",
-    releaseDate: "2019-04-26",
-    poster: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
-  },
-  {
-    title: "Joker",
-    releaseDate: "2019-10-04",
-    poster: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
-  }
-];
+import {movies} from "./data/movies.js"
 function App() {
-
-
   return (
     <>
-        <div className="main-cont bg-[#303030] h-screen overflow-scroll">
-    <nav className='navbar flex justify-between p-4 bg-black'>
-      <div className="navl text-[#736FCE]"> <h5>Movie App</h5></div>
-     
-      <div className="navr flex justify-between w-40 text-[#736FCE]"><h5>Home</h5><h5>Favourites</h5></div>
-    </nav>
-     <div className="search flex justify-between w-[30%]  m-auto mt-10 p-0">
-         <input type="text" placeholder="Search your movie here..."  className="searchbar text-white bg-[#3F3F3F] p-4 w-[70%] rounded-[10px] outline-0"/>
-         <button className="searchbtn text-white bg-red-700 w-[25%] rounded-[10px] shadow-[0_35px_35px_rgba(0,0,0,0.25)]">Search</button>
-     </div>
+      <div className="main-cont bg-[#303030] h-screen overflow-scroll">
+        <nav className="navbar flex justify-between p-4 bg-black">
+          <div className="navl text-[#736FCE]">
+            {" "}
+            <h5>Movie App</h5>
+          </div>
 
-           <div className="movie-card-cont  w-[80%] bg-red-800 h-screen m-auto mt-4 p-4">
-            {
-              movies.forEach((movie)=>{
-                <MovieCard ></MovieCard>
-              })
-            }
-
-           </div>
-
-
+          <div className="navr flex justify-between w-40 text-[#736FCE]">
+            <h5>Home</h5>
+            <h5>Favourites</h5>
+          </div>
+        </nav>
+        <div className="search flex justify-between w-[30%]  m-auto mt-10 p-0">
+          <input
+            type="text"
+            placeholder="Search your movie here..."
+            className="searchbar text-white bg-[#3F3F3F] p-4 w-[70%] rounded-[10px] outline-0"
+          />
+          <button className="searchbtn text-white bg-red-700 w-[25%] rounded-[10px] shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
+            Search
+          </button>
         </div>
+
+        <div className="movie-card-cont  w-[80%]   m-auto mt-4 p-4 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 ">
+          {movies.map((movie,index) => (
+            <MovieCard key={index} title={movie.title} relDate={movie.releaseDate.split('-')[0]} imgPath={movie.posterLink} />
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- projects/movieRecomendation/src/components/movieCard.jsx
+++ projects/movieRecomendation/src/components/movieCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 
 
-function movieCard({imgPath,relDate,name}) {
+function movieCard({imgPath,relDate,title}) {
   return (
-    <div className="movie-cont h-[60%] w-[20%]  bg-[#262626] rounded-[10px] p-0">
-        <img src={imgPath}  className="poster object-cover h-[80%] w-[100%]bg-blue-700 rounded-[10px]"/>
+    <div className="movie-cont  bg-[#262626] rounded-[10px] p-0">
+        <img src={imgPath}  className="poster object-cover h-[80%] w-[100%]  rounded-[10px]"/>
         <div className='info p-4'>
-            <h5 className="self-start text-white mb-2">{name}</h5>
-            <h6 className="rel-date text-[#717171]">{relDate}</h6>
+            <h5 className="self-start text-white ">{title}</h5>
+            <h6 className="rel-date text-[#717171] mb-1">{relDate}</h6>
         </div>
 
 
